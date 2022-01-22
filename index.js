0151342fc5b174d29020c62cfa9971f403d5b094
Swaraj Puppalwar <79976106+UltronTheAI@users.noreply.github.com>
2022-01-22 03:52:08 -0800

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const server = require('http').createServer(app);
 var dataJ = {};
 
 app.get('/', (req, res) => {
+    res.send('error');
     var url = req.url.replace('/?', '');
     url = url.replaceAll('%20', ' ').split(' ');
     // url[1] = url[1].replaceAll('\\"', '"');
